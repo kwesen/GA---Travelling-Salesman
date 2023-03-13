05b5ef84a0f4b946fb7cd84f370bf622096a2163
kwesen <kwesenfeld@gmail.com>
2023-03-13 20:02:05 +0100

    a bit of testing and now double child

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import numpy as np
 
 def cycle_cross_over(P1: list,P2: list) -> list[int]:
-    Offspring: list = [None for i in range(len(P1))]
-    index = 0
-    val_t_find = ''
-    while val_t_find not in Offspring:
-        val_insert = P1[index]
-        Offspring[index] = val_insert
-        val_t_find = P2[index]
-        index = P1.index(val_t_find)
+    def get_child(P1: list,P2: list): 
+        Offspring: list = [None for i in range(len(P1))]
+        index = 0
+        val_t_find = ''
+        while val_t_find not in Offspring:
+            val_insert = P1[index]
+            Offspring[index] = val_insert
+            val_t_find = P2[index]
+            index = P1.index(val_t_find)
 
-    # replace None with P2 values    
-    if None in Offspring:
-        nones = [i for i,val in enumerate(Offspring) if val is None]
-        for i in nones:
-            Offspring[i] = P2[i]
-    return Offspring
+        # replace None with P2 values    
+        if None in Offspring:
+            nones = [i for i,val in enumerate(Offspring) if val is None]
+            for i in nones:
+                Offspring[i] = P2[i]
+        return Offspring
+    return get_child(P1,P2),get_child(P2,P1)
 
 def mutation(P: list) -> list[int]:
     chosen = np.random.choice(P,2, replace=False)
@@ -31,10 +33,10 @@ def mutation(P: list) -> list[int]:
 # P2 = [8,5,6,7,3,4]
 # P1 = [2,4,5,1,3]
 # P2 = [1,5,4,2,3]
-expected = [2,5,4,1,3]
-# P1 = [3,4,1,2,5]
-# P2 = [4,2,5,1,3]
+# expected = [2,5,4,1,3]
+# P1 = [3,4,2,1,5]
+# P2 = [4,1,5,2,3]
 # print(P2)
-cross = cycle_cross_over(P1,P2)
-print(cross)
+# cross = cycle_cross_over(P1,P2)
+# print(cross)
 # test = mutation(P1.copy())
