75242836b716c13fe64bb27db66eb5b21afb317e
kwesen <kwesenfeld@gmail.com>
2023-03-13 19:44:51 +0100

    cycle cross over should be working now

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import numpy as np
 
 def cycle_cross_over(P1: list,P2: list) -> list[int]:
-    O: list = [None for i in range(len(P1))]
-    O[0] = P1[0]
+    Offspring: list = [None for i in range(len(P1))]
+    index = 0
+    val_t_find = ''
+    while val_t_find not in Offspring:
+        val_insert = P1[index]
+        Offspring[index] = val_insert
+        val_t_find = P2[index]
+        index = P1.index(val_t_find)
 
-    val = P2[P1.index(O[0])]
-    index = P1.index(val)
-    val1 = P2[index]
-    O[index] = val 
-
-    while val1 not in O:
-        O[index] = val   
-        val1 = P2[index]
-        val = P1[P2.index(val1)]
-        index = P1.index(val1)
-
-    if None in O:
-        nones = [i for i,val in enumerate(O) if val is None]
+    # replace None with P2 values    
+    if None in Offspring:
+        nones = [i for i,val in enumerate(Offspring) if val is None]
         for i in nones:
-            O[i] = P1[i]
-    return O
+            Offspring[i] = P2[i]
+    return Offspring
 
 def mutation(P: list) -> list[int]:
     chosen = np.random.choice(P,2, replace=False)
@@ -35,8 +31,9 @@ def mutation(P: list) -> list[int]:
 # P2 = [8,5,6,7,3,4]
 # P1 = [2,4,5,1,3]
 # P2 = [1,5,4,2,3]
-P1 = [1,2,3,4,5]
-P2 = [5,2,3,1,4]
+expected = [2,5,4,1,3]
+# P1 = [3,4,1,2,5]
+# P2 = [4,2,5,1,3]
 # print(P2)
 cross = cycle_cross_over(P1,P2)
 print(cross)
