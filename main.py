348de5ab0db417c9ddc49c65bee233acc12256ac
kwesen <kwesenfeld@gmail.com>
2023-03-14 15:39:36 +0100

    Added pop initialization, euclidean distance and city distance map

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ def mutation(P: list) -> list[int]:
     P[i1],P[i2] = P[i2],P[i1]
     return P
 
-x = [0, 3, 6, 7, 15, 12, 14, 9, 7, 0]
-y = [1, 4, 5, 3, 0 ,4, 10, 6, 9, 10]
-plt.plot(x,y,'ro')
-plt.show()
+# x = [0, 3, 6, 7, 15, 12, 14, 9, 7, 0]
+# y = [1, 4, 5, 3, 0 ,4, 10, 6, 9, 10]
+# plt.plot(x,y,'ro')
+# plt.show()
 
 # P1 = [3,4,5,6,7,8]
 # P2 = [8,5,6,7,3,4]
@@ -49,6 +49,7 @@ def city_cord_reader(path: str) -> list[tuple[int, int]]:
     '''Reads the file provided as a path that contains the coordinates of cities\n
     The coordinates should be written in seperate lines in [ ] square
     brackets and separated by spaces'''
+
     with open(path, 'r+') as file:
         usefull_data = file.readlines()[1:]
         ix1,ix2 = usefull_data[0].find('[')+1, usefull_data[0].find(']')
@@ -62,8 +63,31 @@ def city_cord_reader(path: str) -> list[tuple[int, int]]:
 
 cords : list[tuple] = city_cord_reader('Traveling Salesman Problem Data-20230314\cities_1.txt')
 
-for founder in cords:
-    x,y = founder
-    plt.plot(x,y, 'ro')
+# for founder in cords:
+#     x,y = founder
+#     plt.plot(x,y, 'ro')
 
-plt.show()
+# plt.show()
+
+def init_population(n: int, pop: int) -> list[list[int]]:
+    grand_parent = [n for n in range(1,n+1)]
+    parent = []
+    for pop in range(pop):
+        parent.append(np.random.permutation(grand_parent))
+    return parent 
+
+def euclidean_dist(city1: tuple, city2: tuple) -> float:
+    x1, y1 = city1
+    x2, y2 = city2
+    dist = np.sqrt(np.power(x1-x2, 2) + np.power(y1-y2, 2))
+    return dist
+
+def city_dist_map(cities: list[tuple]) -> dict:
+    distance_map = {i : {} for i in range(len(cities))}
+    for key, city in enumerate(cities):
+        for destkey, destination in enumerate(cities):
+            distance_map[key][destkey] = euclidean_dist(city,destination)
+    return distance_map
+
+distances = city_dist_map(cords)
+print(distances[1][2])
